be04958bb22e553b5a6bd9d30711b025b0c738c7
Citlali <gmtlali88@gmail.com>
2018-12-06 17:09:53 -0600

    Exercise for chapter 2

---
+++ chapter2/hello_world2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+    cout<<"Hello, Programming!\n";
+    cout<<"Here we go!\n";
+    return 0;
+}
